bc0f7323e7f01be204bb71d122d11d6ffeee3364
BetLucker <fowziamujtaba@gmail.com>
2025-07-08 10:27:00 +0500

    Add files via upload

---
+++ SagiriRobot/config.py
@@ -0,0 +1,53 @@
+class Config(object):
+    LOGGER = True
+
+    # Get this value from my.telegram.org/apps
+    API_ID = 6
+    API_HASH = "b0efa30bfcf3c419be6cf5db84e62c45"
+
+    CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
+
+    DATABASE_URL = ""  # A sql database url from elephantsql.com
+
+    EVENT_LOGS = ()  # Event logs channel to note down important bot level events
+
+    MONGO_DB_URI = ""  # Get this value from cloud.mongodb.com
+
+    REDIS_URL = ""  #Get this value from redis.com
+
+    # Telegraph link of the image which will be shown at start command.
+    PHOTO = "https://te.legra.ph/file/31077a464a4ca165eb8bc.jpg"
+
+    SUPPORT_CHAT = "yamada_support_chat"  # Your Telegram support group chat username where your users will go and bother you
+
+    TOKEN = ""  # Get bot token from @BotFather on Telegram
+
+    TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
+
+    OWNER_ID = 6171176459  # User id of your telegram account (Must be integer)
+
+    # Optional fields
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    DRAGONS = []  # User id of sudo users
+    DEV_USERS = []  # User id of dev users
+    DEMONS = []  # User id of support users
+    TIGERS = []  # User id of tiger users
+    WOLVES = []  # User id of whitelist users
+
+    ALLOW_CHATS = True
+    ALLOW_EXCL = True
+    DEL_CMDS = True
+    INFOPIC = True
+    LOAD = []
+    NO_LOAD = []
+    STRICT_GBAN = True
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    WORKERS = 8
+
+
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True

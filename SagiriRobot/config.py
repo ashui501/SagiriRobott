8663c7c9ae0e3881d5db001ce25ab71ca3146912
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2025-07-08 13:10:16 +0530

    Update config.py

--- SagiriRobot/config.py
+++ SagiriRobot/config.py
@@ -2,29 +2,29 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 6
-    API_HASH = "b0efa30bfcf3c419be6cf5db84e62c45"
+    API_ID = 21758143
+    API_HASH = "154769775792944f6cd51a77b54c57a7"
 
     CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgresql://neondb_owner:npg_s9chAgJZRWp4@ep-damp-dew-a8fpypno-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require"  # A sql database url from elephantsql.com
 
-    EVENT_LOGS = ()  # Event logs channel to note down important bot level events
+    EVENT_LOGS = (-1002655322719)  # Event logs channel to note down important bot level events
 
-    MONGO_DB_URI = ""  # Get this value from cloud.mongodb.com
+    MONGO_DB_URI = "mongodb+srv://tiwarireeta004:peqxLEd36RAg7ors@cluster0.furypd3.mongodb.net/?retryWrites=true&w=majority"  # Get this value from cloud.mongodb.com
 
     REDIS_URL = ""  #Get this value from redis.com
 
     # Telegraph link of the image which will be shown at start command.
     PHOTO = "https://te.legra.ph/file/31077a464a4ca165eb8bc.jpg"
 
-    SUPPORT_CHAT = "yamada_support_chat"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "ogapexmainchat"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "7770778661:AAGdLJtw3iBNsZILLWVk-WNkBN5bKKgcarA"  # Get bot token from @BotFather on Telegram
 
-    TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
+    TIME_API_KEY = "hagT777"  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 6171176459  # User id of your telegram account (Must be integer)
+    OWNER_ID = 8176785590  # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
